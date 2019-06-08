36a3dbdb25af3a81a12510cf970b9b5854ebdcf6
Regina Alyssa <reginaalyssa01809@gmail.com>
2019-06-08 11:15:39 +0800

    Add document ready call

--- examples/map-reduce-filter/users.js
+++ examples/map-reduce-filter/users.js
@@ -1,16 +1,18 @@
-const url = "https://jsonplaceholder.typicode.com/users";
+$(document).ready(function() {
+	const url = "https://jsonplaceholder.typicode.com/users";
 
-$("#users-button").click(function() {
-	console.log("Click button");
-	$.ajax({
-		url: url,
-		type: "GET",
-		success: function(response) {
-			$("#user-list").text(response);
-			console.log(response);
-		},
-		error: function(error) {
-			console.error(`Error: ${error}`);
-		}
+	$("#users-button").click(function() {
+		console.log("Click button");
+		$.ajax({
+			url: url,
+			type: "GET",
+			success: function(response) {
+				$("#user-list").text(response);
+				console.log(response);
+			},
+			error: function(error) {
+				console.error(`Error: ${error}`);
+			}
+		});
 	});
 });
